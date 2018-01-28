2c1bac63a48695275d5f91f53d5c0e5c9af6dee7
Karl Dackland <c14kdd@cs.umu.se>
2018-01-28 14:40:59 +0100

    assignment3

---
+++ topic2/uppgift3/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String [ ] args) {
+        int iterations[] = {10, 20, 40, 80, 160, 320};
+        int processes[] = {1, 2, 4, 8, 16, 32, 64, 128};
+        System.out.println("\\hline");
+
+        for (int p: processes) {
+            System.out.print(p + "\t");
+            for (int n : iterations) {
+                double ts = calcTS(n); double tp = calcTP(n, p);
+                System.out.print("& " + calcSpeedup(ts, tp) + "\t");
+            }
+            System.out.println("\\\\\\hline");
+        }
+
+        System.out.println("\\hline");
+        for (int p: processes) {
+            System.out.print(p + "\t");
+            for (int n: iterations) {
+                double ts = calcTS(n); double tp = calcTP(n,p);
+                System.out.print("& " + calcSpeedup(ts,tp) + "\t");
+            }
+            System.out.println("\\\\\\hline");
+        }
+    }
+
+    public static double calcTP(int n, int p) {
+        return (Math.pow(n,2)/p) + log(p,2);
+    }
+
+    public static double calcTS(int n) {
+        return Math.pow(n,2);
+    }
+
+    public static float calcE(double ts, double tp, int p) {
+        return (float)(ts/(tp*p));
+    }
+
+    public static float calcSpeedup(double ts, double tp) {
+        return  (float) (ts/tp);
+    }
+
+    static int log(int x, int base)
+    {
+        return (int) (Math.log(x) / Math.log(base));
+    }
+}
